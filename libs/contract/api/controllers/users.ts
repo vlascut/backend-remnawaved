3d9ab4bf9084f60f6ee0b52615df2700846183eb
kastov <kastov@gog.sh>
2024-11-27 19:23:32 +0300

    :construction:
    Co-authored-by: Ivan <84693047+exact01@users.noreply.github.com>

--- libs/contract/api/controllers/users.ts
+++ libs/contract/api/controllers/users.ts
@@ -3,12 +3,12 @@ export const USERS_CONTROLLER = 'users' as const;
 export const USERS_ROUTES = {
     CREATE: '',
     GET_BY_UUID: '',
-    GET_BY_SHORT_UUID: 'short-uuid/:shortUuid',
-    GET_BY_SUBSCRIPTION_UUID: 'sub-uuid/:subscriptionUuid',
+    GET_BY_SHORT_UUID: 'short-uuid',
+    GET_BY_SUBSCRIPTION_UUID: 'sub-uuid',
     GET_ALL: '',
-    REVOKE_SUBSCRIPTION: 'revoke/:uuid',
-    DISABLE_USER: 'disable/:uuid',
-    ENABLE_USER: 'enable/:uuid',
-    DELETE_USER: 'delete/:uuid',
+    REVOKE_SUBSCRIPTION: 'revoke',
+    DISABLE_USER: 'disable',
+    ENABLE_USER: 'enable',
+    DELETE_USER: 'delete',
     UPDATE: 'update',
 } as const;
