2eb6c628ffb777e792d54e54d449874570dcdd7f
kastov <kastov@gog.sh>
2024-12-03 18:07:13 +0300

    feat: test stats
    Co-authored-by: Ivan <84693047+exact01@users.noreply.github.com>

--- libs/contract/commands/users/get-all-users-v2.command.ts
+++ libs/contract/commands/users/get-all-users-v2.command.ts
@@ -3,24 +3,34 @@ import { REST_API } from '../../api';
 import { UsersSchema } from '../../models/users.schema';
 
 export namespace GetAllUsersV2Command {
-    export const url = REST_API.USERS.GET_ALL;
+    export const url = REST_API.USERS.GET_ALL_V2;
 
     export const RequestQuerySchema = z.object({
-        start: z.number().optional(),
-        size: z.number().optional(),
+        start: z.coerce.number().optional(),
+        size: z.coerce.number().optional(),
         filters: z
-            .array(
-                z.object({
-                    id: z.string(),
-                    value: z.string(),
-                }),
+            .string()
+            .transform((str) => JSON.parse(str))
+            .pipe(
+                z.array(
+                    z.object({
+                        id: z.string(),
+                        value: z.string(),
+                    }),
+                ),
             )
             .optional(),
         filterModes: z
-            .record(z.string(), z.enum(['contains', 'startsWith', 'endsWith']))
+            .string()
+            .transform((str) => JSON.parse(str))
+            .pipe(z.record(z.string(), z.string()))
             .optional(),
         globalFilterMode: z.string().optional(),
-        sorting: z.array(z.object({ id: z.string(), desc: z.boolean() })).optional(),
+        sorting: z
+            .string()
+            .transform((str) => JSON.parse(str))
+            .pipe(z.array(z.object({ id: z.string(), desc: z.boolean() })))
+            .optional(),
     });
 
     export type RequestQuery = z.infer<typeof RequestQuerySchema>;
