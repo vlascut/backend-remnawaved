4f542bc5653108b18feb01a3999319a97aa7efed
kastov <kastov@gog.sh>
2024-12-05 23:44:13 +0300

    :construction: update for TSQ
    Co-authored-by: Ivan <84693047+exact01@users.noreply.github.com>

--- libs/contract/commands/nodes/update.command.ts
+++ libs/contract/commands/nodes/update.command.ts
@@ -8,45 +8,25 @@ export namespace UpdateNodeCommand {
 
     export const RequestSchema = NodesSchema.pick({
         uuid: true,
-    })
-        .extend({
-            name: z.optional(z.string().min(5, 'Min. 5 characters')),
-            address: z.optional(z.string().min(2, 'Min. 2 characters')),
-            port: z.optional(z.number()),
-            isTrafficTrackingActive: z.optional(z.boolean()),
-            trafficLimitBytes: z.optional(
-                z.number().min(0, 'Traffic limit must be greater than 0'),
-            ),
-            notifyPercent: z.optional(
-                z
-                    .number()
-                    .min(0, 'Notify percent must be greater than 0')
-                    .max(100, 'Notify percent must be less than 100'),
-            ),
-            trafficResetDay: z.optional(
-                z
-                    .number()
-                    .min(1, 'Traffic reset day must be greater than 0')
-                    .max(31, 'Traffic reset day must be less than 31'),
-            ),
-        })
-        .refine(
-            (data) => {
-                if (data.isTrafficTrackingActive) {
-                    return (
-                        data.trafficLimitBytes != null &&
-                        data.notifyPercent != null &&
-                        data.trafficResetDay != null
-                    );
-                }
-                return true;
-            },
-            {
-                message:
-                    'Traffic tracking fields are required when isTrafficTrackingActive is true',
-                path: ['trafficLimitBytes', 'notifyPercent', 'trafficResetDay'],
-            },
-        );
+    }).extend({
+        name: z.optional(z.string().min(5, 'Min. 5 characters')),
+        address: z.optional(z.string().min(2, 'Min. 2 characters')),
+        port: z.optional(z.number()),
+        isTrafficTrackingActive: z.optional(z.boolean()),
+        trafficLimitBytes: z.optional(z.number().min(0, 'Traffic limit must be greater than 0')),
+        notifyPercent: z.optional(
+            z
+                .number()
+                .min(0, 'Notify percent must be greater than 0')
+                .max(100, 'Notify percent must be less than 100'),
+        ),
+        trafficResetDay: z.optional(
+            z
+                .number()
+                .min(1, 'Traffic reset day must be greater than 0')
+                .max(31, 'Traffic reset day must be less than 31'),
+        ),
+    });
 
     export type Request = z.infer<typeof RequestSchema>;
 
