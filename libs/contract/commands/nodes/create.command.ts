7adfcb96dd98267d19a2528175955d6face7cb61
kastov <kastov@gog.sh>
2024-12-05 23:41:23 +0300

    :construction: update for TSQ
    Co-authored-by: Ivan <84693047+exact01@users.noreply.github.com>

--- libs/contract/commands/nodes/create.command.ts
+++ libs/contract/commands/nodes/create.command.ts
@@ -6,28 +6,28 @@ export namespace CreateNodeCommand {
     export const url = REST_API.NODES.CREATE;
     export const TSQ_url = url;
 
-    export const RequestSchema = NodesSchema.pick({}).extend({
+    export const RequestSchema = z.object({
         name: z.string().min(5, 'Name is required'),
         address: z.string().min(2, 'Address is required'),
         port: z.number().int().min(1, 'Port is required').optional(),
         isTrafficTrackingActive: z.boolean().optional().default(false),
-        trafficLimitBytes: z
-            .number()
-            .int()
-            .min(0, 'Traffic limit must be greater than 0')
-            .optional(),
-        notifyPercent: z
-            .number()
-            .int()
-            .min(0, 'Notify percent must be greater than 0')
-            .max(100, 'Notify percent must be less than 100')
-            .optional(),
-        trafficResetDay: z
-            .number()
-            .int()
-            .min(1, 'Traffic reset day must be greater than 0')
-            .max(31, 'Traffic reset day must be less than 31')
-            .optional(),
+        trafficLimitBytes: z.optional(
+            z.number().int().min(0, 'Traffic limit must be greater than 0'),
+        ),
+        notifyPercent: z.optional(
+            z
+                .number()
+                .int()
+                .min(0, 'Notify percent must be greater than 0')
+                .max(100, 'Notify percent must be less than 100'),
+        ),
+        trafficResetDay: z.optional(
+            z
+                .number()
+                .int()
+                .min(1, 'Traffic reset day must be greater than 0')
+                .max(31, 'Traffic reset day must be less than 31'),
+        ),
     });
 
     export type Request = z.infer<typeof RequestSchema>;
