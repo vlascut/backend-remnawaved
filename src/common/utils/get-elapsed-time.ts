86fb934beee8519d24f9d0a291e0e5fcb3115dec
kastov <kastov@gog.sh>
2024-12-18 07:10:02 +0300

    chore: add TODO comment to formatExecutionTime function for future removal
    - Added a TODO comment in the formatExecutionTime function to indicate that the current implementation should be reviewed and potentially removed in the future.

--- src/common/utils/get-elapsed-time.ts
+++ src/common/utils/get-elapsed-time.ts
@@ -1,6 +1,7 @@
 import prettyMs from '@minikit/pretty-ms';
 
 export function formatExecutionTime(startTime: number): string {
+    // TODO: Remove this
     return prettyMs(Date.now() - startTime);
 }
 
