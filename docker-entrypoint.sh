395720f92247c87b9be0734596881c0aff5036b2
kastov <kastov@gog.sh>
2025-02-19 01:17:20 +0300

    chore: configure PM2 logging and web interface

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,6 @@ echo "Migrating database..."
 npm run migrate:deploy
 
 echo "Starting pm2..."
-pm2-runtime start ecosystem.config.js --env=production
+pm2-runtime start ecosystem.config.js --env=production --web 5566
 
 echo "Entrypoint script completed."
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
             script: 'dist/src/main.js',
             watch: false,
             instances: process.env.INSTANCES || 1,
+            merge_logs: true,
             exec_mode: 'cluster',
             env_development: {
                 NODE_ENV: 'development',
@@ -17,8 +18,9 @@ module.exports = {
             name: 'remnawave-jobs',
             script: 'dist/src/worker.js',
             watch: false,
-            instances: 1, // DO NOT CHANGE THIS
+            instances: 1, // DO NOT SCALE WORKER
             exec_mode: 'fork',
+            merge_logs: true,
             env_development: {
                 NODE_ENV: 'development',
             },
