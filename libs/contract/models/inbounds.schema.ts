256cd25098174a61cd3feee32f9867c03aa96bce
kastov <kastov@gog.sh>
2025-03-21 18:37:58 +0300

    chore: bump version to 0.3.55 and update Inbounds schema
    - Updated version in package.json to 0.3.55.
    - Added nullable fields 'network' and 'security' to InboundsSchema for enhanced data structure.
    - Updated GetInboundsResponseModel to include new fields for better response handling.

--- libs/contract/models/inbounds.schema.ts
+++ libs/contract/models/inbounds.schema.ts
@@ -4,4 +4,6 @@ export const InboundsSchema = z.object({
     uuid: z.string().uuid(),
     tag: z.string(),
     type: z.string(),
+    network: z.nullable(z.string()),
+    security: z.nullable(z.string()),
 });
