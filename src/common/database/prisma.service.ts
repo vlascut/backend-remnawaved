004d4cdfb298ccd88142f682baf1b51a392e60d5
kastov <kastov@gog.sh>
2025-04-04 22:15:38 +0300

    fix: remove debug logging

--- src/common/database/prisma.service.ts
+++ src/common/database/prisma.service.ts
@@ -5,7 +5,7 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     constructor() {
-        super({ log: ['query'] });
+        super();
     }
     async onModuleInit() {
         await this.$connect();
