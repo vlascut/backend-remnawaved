fcf4d8abeaa95714b36816b36aed42347f867ba6
kastov <kastov@gog.sh>
2025-06-20 06:17:06 +0300

    chore: update Dockerfile

--- types.d.ts
+++ types.d.ts
@@ -3,14 +3,12 @@ import { Kysely } from 'kysely';
 
 import { DB } from 'prisma/generated/types';
 
-// Расширяем PrismaClient для транзакций
 declare module '@prisma/client' {
     interface PrismaClient {
         $kysely: Kysely<DB>;
     }
 }
 
-// // Альтернативно, если нужно для транзакционного адаптера:
 // declare module '@nestjs-cls/transactional-adapter-prisma' {
 //     interface TransactionalAdapterPrisma {
 //         tx: {
