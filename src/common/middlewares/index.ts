b7c9758a9fcb886e38126b3d7d36022bccf90c6c
kastov <kastov@gog.sh>
2025-08-14 17:14:53 +0300

    feat: integrate noRobotsMiddleware into application

--- src/common/middlewares/index.ts
+++ src/common/middlewares/index.ts
@@ -1,4 +1,5 @@
 export * from './basic-auth.middleware';
 export * from './check-auth-cookie.middleware';
 export * from './get-real-ip';
+export * from './no-robots.middleware';
 export * from './proxy-check.middleware';
