58c87f05f2f2ed38144242c5b19b4bf785bc63a1
Ivan.Nginx <ivan.nginx@gmail.com>
2025-08-17 23:20:50 +0300

    fix: update no-robots middleware to enhance search engine indexing directives (#89)

--- src/common/middlewares/no-robots.middleware.ts
+++ src/common/middlewares/no-robots.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function noRobotsMiddleware(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('x-robots-tag', 'noindex, follow');
+    res.setHeader('x-robots-tag', 'noindex, nofollow, noarchive, nosnippet, noimageindex');
 
     return next();
 }
