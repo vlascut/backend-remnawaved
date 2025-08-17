80bbdddd9dfef11c657c866b7b70d3e9be8f9890
kastov <kastov@gog.sh>
2025-08-17 14:30:17 +0300

    chore: update Dockerfile and package.json for Prisma configuration

--- src/common/utils/filter-logs/filter-logs.ts
+++ src/common/utils/filter-logs/filter-logs.ts
@@ -5,6 +5,10 @@ const contextsToIgnore = ['InstanceLoader', 'RoutesResolver', 'RouterExplorer'];
 export const customLogFilter = winston.format((info) => {
     if (info.context) {
         const contextValue = String(info.context);
+        if (contextValue.endsWith('Queue')) {
+            return false;
+        }
+
         if (contextsToIgnore.some((ctx) => contextValue === ctx)) {
             return false;
         }
--- src/common/utils/startup-app/get-start-message.ts
+++ src/common/utils/startup-app/get-start-message.ts
@@ -1,11 +1,12 @@
 import { getBorderCharacters, table } from 'table';
 import { readPackageJSON } from 'pkg-types';
 
-export async function getStartMessage() {
+export async function getStartMessage(appPort: number, metricsPort: number) {
     const pkg = await readPackageJSON();
 
     return table(
         [
+            [`API Port: ${appPort} | Metrics Port: ${metricsPort}`],
             ['Docs → https://remna.st\nCommunity → https://t.me/remnawave'],
             ['Rescue CLI → docker exec -it remnawave remnawave'],
         ],
